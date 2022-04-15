9982f9126eca55c913f0b5b0cbd514260073adab
bhaktibuana <bhaktibuana19@gmail.com>
2022-04-15 21:36:53 +0700

    import rating route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const recipeRouter = require("./recipe.route");
 const userRouter = require("./user.route");
+const ratingRouter = require("./rating.route");
 
 const router = express.Router();
 
 router.use("/", recipeRouter);
 router.use("/", userRouter);
+router.use("/", ratingRouter);
 
 router.use("/", (req, res) => {
   res.status(200).json({
