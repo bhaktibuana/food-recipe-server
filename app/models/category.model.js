12f79bbad002a4aa620017c7f2bfd0d088d1b924
bhaktibuana <bhaktibuana19@gmail.com>
2022-04-15 21:38:44 +0700

    create query for add category

---
+++ app/models/category.model.js
@@ -0,0 +1,16 @@
+const db = require("../../config/dbConnection");
+
+const selectByName = (params, callback) => {
+  const selectQuery = `SELECT id, name FROM categories WHERE is_deleted = FALSE AND name = ?;`;
+  db.query(selectQuery, params, callback);
+};
+
+const create = (params, callback) => {
+  const insertQuery = `INSERT INTO categories (name) VALUES (?);`;
+  db.query(insertQuery, params, callback);
+};
+
+module.exports = {
+  selectByName,
+  create,
+};
